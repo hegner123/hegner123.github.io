89378d874a56b27366c99823f85c9e253f9ba603
Michael Hegner <hegner123@gmail.com>
2019-10-01 07:36:20 -0400

    added multiplayer RPS, some style changes

--- assets/javascript/javascript.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function (){
-  $('#text-area').on('keyup', function(event){
-    if (event.keyCode === 13) {
-      event.preventDefault();
-     $("#submit-button").click();
-    };
-  })
-})
